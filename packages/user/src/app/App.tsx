4db95d0f144ea3203b233a476b6e71f11fbdaac0
uvarovag <aleanches@gmail.com>
2025-02-15 00:14:40 +0300

    chore: update dependencies

--- packages/user/src/app/App.tsx
+++ packages/user/src/app/App.tsx
@@ -6,7 +6,9 @@ import { routes } from './routes'
 import type { ProviderParams } from '@module-federation/bridge-react'
 
 export const App = ({ basename }: ProviderParams) => {
-    const router = createBrowserRouter(routes, { basename })
+    const router = createBrowserRouter(routes, {
+        basename,
+    })
     return (
         <Providers>
             <RouterProvider router={router} />
--- packages/user/src/widgets/Navbar/Navbar.tsx
+++ packages/user/src/widgets/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => (
             <a href="/document">navigate external to remote document</a>
         </li>
         <li>
-            <Link to="../">main</Link>
+            <Link to="/">main</Link>
         </li>
     </ul>
 )
